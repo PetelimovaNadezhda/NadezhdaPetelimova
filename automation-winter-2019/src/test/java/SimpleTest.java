4c0d201fd4cf505b09ab15ccf45233552b7cef06
Nadezhda Petelimova <hikari.aka.nadya@yandex.ru>
2019-01-23 18:41:09 +0300

    First simple test

--- automation-winter-2019/src/test/java/SimpleTest.java
+++ automation-winter-2019/src/test/java/SimpleTest.java
@@ -1,11 +1,22 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class SimpleTest {
 
+    @Test
     public void simpleTest(){
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
         driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        assertEquals(driver.getTitle(), "Home Page");
+
+        driver.close();
     }
 
 }
