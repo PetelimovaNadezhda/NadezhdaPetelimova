47df0b871b33099cd749a6aa2938490212866d06
Nadezhda Petelimova <hikari.aka.nadya@yandex.ru>
2019-01-27 14:39:10 +0300

    Add tasks of Homework2.Ex1

--- automation-winter-2019/src/test/java/hw1/EpamSiteSoftAssertTest.java
+++ automation-winter-2019/src/test/java/hw1/EpamSiteSoftAssertTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 
-public class EpamSiteSoftAssertTest extends SeleniumBase {
+public class EpamSiteSoftAssertTest extends SeleniumBase{
 
     @Test
     public void testEpamJdiPage() {
