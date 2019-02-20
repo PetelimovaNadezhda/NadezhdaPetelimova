0f2409410e3a7ad438d190271c948b43fcbd67ab
Nadezhda Petelimova <hikari.aka.nadya@yandex.ru>
2019-02-20 19:10:37 +0300

    Add task of Homework6

--- src/main/java/homework/hw6/Superhero.java
+++ src/main/java/homework/hw6/Superhero.java
@@ -1,29 +1,19 @@
 package homework.hw6;
 
-import java.util.Objects;
-
-import static java.lang.String.*;
-
 // TODO Basically, this is not the 'Table' at all.
 // TODO What is the business sense on this entity ?
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@EqualsAndHashCode
 public class Superhero {
-    public static final String TEMPLATE = "%s%s%s";
-
-    private Integer number;
-    private String user;
-    private String description;
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
+    Integer number;
+    String user;
+    String description;
 
     public Superhero(String numberType, String user, String description) {
         this.number = Integer.parseInt(numberType);
@@ -31,24 +21,4 @@ public class Superhero {
         this.description = description.replaceAll("[\n ]", " ");
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Superhero table = (Superhero) o;
-        return Objects.equals(number, table.number) &&
-                Objects.equals(user, table.user) &&
-                Objects.equals(description, table.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number, user, description);
-    }
-
-    @Override
-    public String toString() {
-        return format(TEMPLATE, number, user, description)
-                .replaceAll("[\n ]", "");
-    }
 }
